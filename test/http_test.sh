a6fc372b002faacef7c8634c436a5090cf270817
chimaoshu <jach3n@gmail.com>
2023-03-30 15:03:11 +0800

    test: test script

--- test/http_test.sh
+++ test/http_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 url="http://127.0.0.1:8080/api"
-num_requests=1000
-concurrency=100
+num_requests=100
+concurrency=50
 
 # 发送请求并记录时间
 time for ((i=1;i<=$num_requests;i++)); do
-  curl -s -o /dev/null -w "%{http_code}\n" -X GET "$url" &
+  curl -s -o /dev/null -w "%{http_code}\n" -X POST "$url" -D "ping" &
   if (( $i % $concurrency == 0 )); then
     wait
   fi
