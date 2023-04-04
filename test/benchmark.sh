26034443ce69fc00e0de593a210fb43ce21fdc7d
chimaoshu <jach3n@gmail.com>
2023-04-04 08:19:03 -0700

    test: benchmark script

---
+++ test/benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in 64KB 4K 16K 64K 128K 256K 512K 1M 4M 16M 128M 1G;
+do
+
+# run web_server
+cd ../bin/
+./web_server &
+pid=$!
+sleep 5
+cd -
+
+# benchmark
+wrk -t4 -c1000 -d60s http://127.0.0.1:8080/${i}.txt > ${i}.benchmark
+
+# kill
+kill -9 $pid
+done
