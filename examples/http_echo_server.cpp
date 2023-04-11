37b2ac24c53c67a1479a11bc1cb32309a381e5f9
chimaoshu <jach3n@gmail.com>
2023-04-11 16:56:34 +0800

    feat: update echo server

--- examples/http_echo_server.cpp
+++ examples/http_echo_server.cpp
@@ -1,18 +1,13 @@
 #include "worker.h"
 
 int echo_handler(http::request<http::string_body> &req,
-                 std::variant<http::response<http::string_body>,
-                              http::response<http::buffer_body>> &variant_res,
+                 http::response<http::string_body> &res,
                  process_func_args &args)
 {
-  variant_res = http::response<http::string_body>{};
-  auto &res = std::get<http::response<http::string_body>>(variant_res);
   res.result(http::status::ok);
   res.set(http::field::content_type, "text/html");
-  res.keep_alive(false);
-  res.body() = req.body();
+  res.body() = std::move(req.body());
   res.prepare_payload();
-
   return 0;
 }
 
